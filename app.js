4a7b850e387cf7633e6664fc8c18e3c45b3f36e1
Jorge Cuevas <jcq012@gmail.com>
2018-01-06 16:58:04 -0400

    some comment added

--- app.js
+++ app.js
@@ -8,30 +8,32 @@ app.set('view engine', 'ejs')
 
 const port = 3000 || process.env.PORT
 
+//configuration , you should use your own keys
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
-    'client_id': keys.clientKey,
-    'client_secret': keys.secretKey
+    'client_id': keys.clientKey, // client key
+    'client_secret': keys.secretKey // secrect key
 });
+
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.post('/pay', (req, res) => {
-
+    // create a valid payment object
     var create_payment_json = {
         "intent": "sale",
         "payer": {
             "payment_method": "paypal"
         },
         "redirect_urls": {
-            "return_url": "http://localhost:3000/success",
-            "cancel_url": "http://localhost:3000/cancel"
+            "return_url": "http://localhost:3000/success", // uri where to go after payment is done succetly
+            "cancel_url": "http://localhost:3000/cancel" // something when wrong 
         },
         "transactions": [
             {
-                "item_list": {
+                "item_list": { // item information  and quantity
                     "items": [
                         {
                             "name": "Red Soc Hat",
@@ -42,7 +44,7 @@ app.post('/pay', (req, res) => {
                         }
                     ]
                 },
-                "amount": {
+                "amount": { // price of item
                     "currency": "USD",
                     "total": "25.00"
                 },
@@ -51,19 +53,19 @@ app.post('/pay', (req, res) => {
         ]
     };
 
-    paypal
-        .payment
-        .create(create_payment_json, function (error, payment) {
+    //create payment
+    paypal.payment.create(create_payment_json, function (error, payment) {
             if (error) {
                 throw error;
             } else {
                 console.log("Create Payment Response");
                 console.log(payment);
+                // if everything go well then redirect to paypal login
                 payment
                     .links
                     .forEach((link) => {
                         if (link.rel === 'approval_url') {
-                            res.redirect(link.href)
+                            res.redirect(link.href) // take the uri to approval login payment
                         }
                     })
 
@@ -76,6 +78,7 @@ app.get('/success', (req, res) => {
     let payerId = req.query.PayerID
     let paymentId = req.query.paymentId
 
+    // confirm payment 
     const execute_payment_json = {
         "payer_id": payerId,
         "transactions": [
@@ -87,13 +90,14 @@ app.get('/success', (req, res) => {
             }
         ]
     }
-
+    // execute payment and do your own transation in your database
     paypal.payment.execute(paymentId, execute_payment_json, (error, payment) => {
         if (error) {
             console.log('something went wrong')
             throw error
         } else {
-            console.log(JSON.stringify(payment))
+            // if everything go well , you have access to your payment information do some stuff to your store if you need it
+            console.log(JSON.stringify(payment)) 
             res.send('success')
         }
     })
